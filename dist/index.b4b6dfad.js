5ef15ffd26bfad9d60bee51d0fb706a0e8040d36
Kresna Sucandra <kresnasucandra@gmail.com>
2022-04-24 21:47:08 +0800

    Feat: Added componentDidMount to query API

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -24988,34 +24988,18 @@ class MainView extends _reactDefault.default.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                {
-                    _id: 1,
-                    Title: 'Inception',
-                    Description: 'Inception is a 2010 stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets. He is offered a chance to have his criminal history erased as payment for the implantation of another person idea into a target subconscious.',
-                    ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg',
-                    Genre: 'Science Fiction',
-                    Director: 'Christopher Nolan'
-                },
-                {
-                    _id: 2,
-                    Title: 'The Shawshank Redemption',
-                    Description: 'The Shawshank Redemption is a 1994 American film based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. It tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence.',
-                    ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
-                    Genre: 'Drama',
-                    Director: 'Frank Darabont'
-                },
-                {
-                    _id: 3,
-                    Title: 'Gladiator',
-                    Description: 'Gladiator is a 2000 epic historical drama film stars Russell Crowe, portrays Roman general Maximus Decimus Meridius, who is betrayed when Commodus, the ambitious son of Emperor Marcus Aurelius, murders his father and seizes the throne. Reduced to slavery, Maximus becomes a gladiator and rises through the ranks of the arena to avenge the murders of his family and his emperor.',
-                    ImagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Gladiator_%282000_film_poster%29.png/220px-Gladiator_%282000_film_poster%29.png',
-                    Genre: 'Drama',
-                    Director: 'Ridley Scott '
-                }, 
-            ],
+            movies: [],
             selectedMovie: null
         };
+    }
+    componentDidMount() {
+        _axiosDefault.default.get('https://my-flix-93462.herokuapp.com/movies').then((response)=>{
+            this.setState({
+                movies: response.data
+            });
+        }).catch((error)=>{
+            console.error(error);
+        });
     }
     setSelectedMovie(newSelectedMovie) {
         this.setState({
@@ -25025,12 +25009,11 @@ class MainView extends _reactDefault.default.Component {
     render() {
         let { movies , selectedMovie  } = this.state;
         if (movies.length === 0) return /*#__PURE__*/ _jsxDevRuntime.jsxDEV("div", {
-            className: "main-view",
-            children: "The list is empty!"
+            className: "main-view"
         }, void 0, false, {
             fileName: "src/components/main-view/main-view.jsx",
-            lineNumber: 55,
-            columnNumber: 14
+            lineNumber: 36,
+            columnNumber: 37
         }, this);
         return /*#__PURE__*/ _jsxDevRuntime.jsxDEV("div", {
             className: "main-view",
@@ -25041,7 +25024,7 @@ class MainView extends _reactDefault.default.Component {
                 }
             }, void 0, false, {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 60,
+                lineNumber: 41,
                 columnNumber: 11
             }, this) : movies.map((movie1)=>/*#__PURE__*/ _jsxDevRuntime.jsxDEV(_movieCard.MovieCard, {
                     movie: movie1,
@@ -25050,13 +25033,13 @@ class MainView extends _reactDefault.default.Component {
                     }
                 }, movie1._id, false, {
                     fileName: "src/components/main-view/main-view.jsx",
-                    lineNumber: 68,
+                    lineNumber: 49,
                     columnNumber: 13
                 }, this)
             )
         }, void 0, false, {
             fileName: "src/components/main-view/main-view.jsx",
-            lineNumber: 58,
+            lineNumber: 39,
             columnNumber: 7
         }, this);
     }
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,40 +8,22 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Inception',
-          Description:
-            'Inception is a 2010 stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets. He is offered a chance to have his criminal history erased as payment for the implantation of another person idea into a target subconscious.',
-          ImagePath:
-            'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg',
-          Genre: 'Science Fiction',
-          Director: 'Christopher Nolan',
-        },
-        {
-          _id: 2,
-          Title: 'The Shawshank Redemption',
-          Description:
-            'The Shawshank Redemption is a 1994 American film based on the 1982 Stephen King novella Rita Hayworth and Shawshank Redemption. It tells the story of banker Andy Dufresne (Tim Robbins), who is sentenced to life in Shawshank State Penitentiary for the murders of his wife and her lover, despite his claims of innocence.',
-          ImagePath:
-            'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
-          Genre: 'Drama',
-          Director: 'Frank Darabont',
-        },
-        {
-          _id: 3,
-          Title: 'Gladiator',
-          Description:
-            'Gladiator is a 2000 epic historical drama film stars Russell Crowe, portrays Roman general Maximus Decimus Meridius, who is betrayed when Commodus, the ambitious son of Emperor Marcus Aurelius, murders his father and seizes the throne. Reduced to slavery, Maximus becomes a gladiator and rises through the ranks of the arena to avenge the murders of his family and his emperor.',
-          ImagePath:
-            'https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Gladiator_%282000_film_poster%29.png/220px-Gladiator_%282000_film_poster%29.png',
-          Genre: 'Drama',
-          Director: 'Ridley Scott ',
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://my-flix-93462.herokuapp.com/movies')
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -51,8 +33,7 @@ export class MainView extends React.Component {
   render() {
     let { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0)
-      return <div className='main-view'>The list is empty!</div>;
+    if (movies.length === 0) return <div className='main-view' />;
 
     return (
       <div className='main-view'>
