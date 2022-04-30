04f2112ab5b04944dcf2c346aa111bd22aeb155e
Kresna Sucandra <kresnasucandra@gmail.com>
2022-05-01 03:15:28 +0800

    Chore: implement Card into MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import './movie-card.scss';
 
@@ -8,18 +10,26 @@ export class MovieCard extends React.Component {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <div
-        className='movie-card'
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        <div>
-          {movie.Title}
-          <div>{movie.ReleaseYear}</div>
-          <br />
-        </div>
-      </div>
+      // <div
+      //   className='movie-card'
+      //   onClick={() => {
+      //     onMovieClick(movie);
+      //   }}
+      // >
+      //   <div>
+      //     {movie.Title}
+      //     <div>{movie.ReleaseYear}</div>
+      //     <br />
+      //   </div>
+      // </div>
+      <Card>
+        <Card.Img variant='top' src={movie.ImagePath}/>
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} >Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
